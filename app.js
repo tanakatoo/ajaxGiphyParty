d66f044356a02cbdf94453f8035bc9e0e548689b
Karmen Too <karmen.tanaka@gmail.com>
2022-09-23 15:24:58 -0400

    done

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Let's get this party started!");
+$('#getit').on("click", async (e) => {
+    e.preventDefault()
+    const searchTerm = $('#searchTerm').val()
+    console.log(searchTerm)
+    const para = { q: searchTerm }
+    const res = await axios.get('https://api.giphy.com/v1/gifs/search?api_key=w2QnH4mnx2CyVaZCsR5m88cFg3lZRRTa', { params: para })
+    
+    for (let img of res.data.data) {
+        $("#boxForGif").append(`<div class='col'><img src='${img.images.downsized.url}'></div>`)
+        //
+    }
+})
+
+$('#removeit').on("click", function (e) {
+    e.preventDefault()
+    $('#boxForGif').empty()
+})
